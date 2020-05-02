50ba61af99c68e87facfba5c7279fc14e1439876
Rajas Kulkarni <rajas.aveo@gmail.com>
2020-05-02 18:20:01 +0530

    added google/email auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,38 @@
-import 'dart:io';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gallery_saver/gallery_saver.dart';
-import 'package:image_picker/image_picker.dart';
 
+import 'ui/home.dart';
+import 'ui/login.dart';
+import 'ui/splash.dart';
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String firstButtonText = 'Take photo';
-  String secondButtonText = 'Record video';
-  double textSize = 20;
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 1,
-              child: Container(
-                child: SizedBox.expand(
-                  child: RaisedButton(
-                    color: Colors.blue,
-                    onPressed: _takePhoto,
-                    child: Text(firstButtonText,
-                        style:
-                            TextStyle(fontSize: textSize, color: Colors.white)),
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              child: Container(
-                  child: SizedBox.expand(
-                child: RaisedButton(
-                  color: Colors.white,
-                  onPressed: _recordVideo,
-                  child: Text(secondButtonText,
-                      style: TextStyle(
-                          fontSize: textSize, color: Colors.blueGrey)),
-                ),
-              )),
-              flex: 1,
-            )
-          ],
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    ));
+      home: MainScreen(),
+    );
   }
+}
 
-  void _takePhoto() async {
-    ImagePicker.pickImage(source: ImageSource.camera)
-        .then((File recordedImage) {
-      if (recordedImage != null && recordedImage.path != null) {
-        setState(() {
-          firstButtonText = 'Saving in progress...';
-        });
-        GallerySaver.saveImage(recordedImage.path).then((path) {
-          setState(() {
-            firstButtonText = 'Image Saved, Capture another?';
-          });
-        });
-      }
-    });
-  }
 
-  void _recordVideo() async {
-    ImagePicker.pickVideo(source: ImageSource.camera)
-        .then((File recordedVideo) {
-      if (recordedVideo != null && recordedVideo.path != null) {
-        setState(() {
-          secondButtonText = 'saving in progress...';
-        });
-        GallerySaver.saveVideo(recordedVideo.path).then((path) {
-          setState(() {
-            secondButtonText = 'Video Saved, Capture another?';
-          });
-        });
-      }
-    });
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context,AsyncSnapshot<FirebaseUser> snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting)
+          return SplashPage();
+        if(!snapshot.hasData || snapshot.data == null)
+          return LoginPage();
+        return HomePage();
+      },
+    );
   }
 }
