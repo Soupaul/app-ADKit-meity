fc31e6eef94274807a3f3eb53a7c3556d5a30d3a
Rajas Kulkarni <rajas.aveo@gmail.com>
2020-05-04 01:17:07 +0530

    UI Changes

--- lib/utils/firebase_auth.dart
+++ lib/utils/firebase_auth.dart
@@ -27,15 +27,6 @@ class AuthProvider {
   }
 
 
- Future<String> getCurrentUser() async {
-    final FirebaseUser user = await _auth.currentUser();
-    final uid = user.uid;
-    // Similarly we can get email as well
-    //final uemail = user.email;
-    return uid.toString();
-    //print(uemail);
-  }
-
   Future<bool> loginWithGoogle() async {
     try {
       GoogleSignIn googleSignIn = GoogleSignIn();
@@ -54,5 +45,38 @@ class AuthProvider {
       print("Error logging with google");
       return false;
     }
+  }
+
+  Future<String> signIn(String email, String password) async {
+    AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    FirebaseUser user = result.user;
+    return user.uid;
+  }
+
+  Future<String> signUp(String email, String password) async {
+    AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    FirebaseUser user = result.user;
+    return user.uid;
+  }
+
+  Future<FirebaseUser> getCurrentUser() async {
+    FirebaseUser user = await _auth.currentUser();
+    return user;
+  }
+
+  Future<void> signOut() async {
+    return _auth.signOut();
+  }
+
+  Future<void> sendEmailVerification() async {
+    FirebaseUser user = await _auth.currentUser();
+    user.sendEmailVerification();
+  }
+
+  Future<bool> isEmailVerified() async {
+    FirebaseUser user = await _auth.currentUser();
+    return user.isEmailVerified;
   }
 }
