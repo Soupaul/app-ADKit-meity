1a13699dfeacb44355ec0db244d216de81809078
subhadiprudra <neloy.ne2@gmail.com>
2021-11-03 14:25:44 +0530

    chatbot updated

--- lib/ui/firestore_form.dart
+++ lib/ui/firestore_form.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
@@ -88,24 +89,60 @@ class _FirestoreFormState extends State<FirestoreForm> {
   }
 
   List list = [];
+  List subQuestions = [];
   List checkList = [];
-  List selectedSym = [];
+  List selectedSymToTravarse = [];
+  List allSelectedSym = [];
   int state = 0;
-  Map<String, int> wtMap = new Map();
+  int subState = 0;
+  bool isEnded = false;
+  bool isAtStart = true;
+  Map ansMap = new Map();
+  var wtMap;
 
   Future<void> getData() async {
     FirebaseFirestore.instance
-        .collection('chatbot')
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((value) {
+      print(value['gender']);
+
+      FirebaseFirestore.instance
+          .collection('symptoms')
+          .get()
+          .then((QuerySnapshot querySnapshot) {
+        setState(() {
+          List temp = querySnapshot.docs;
+          temp.forEach((element) {
+            try {
+              if (element['sex'] == value['gender']) {
+                list.add(element);
+              }
+            } catch (e) {
+              list.add(element);
+            }
+          });
+
+          checkList = List.filled(list.length, false);
+        });
+      });
+    });
+
+    FirebaseFirestore.instance
+        .collection('chatbot_subquestions')
         .get()
         .then((QuerySnapshot querySnapshot) {
-      setState(() {
-        list = querySnapshot.docs;
-        checkList = List.filled(list.length, false);
-      });
+      subQuestions = querySnapshot.docs;
+    });
 
-      // list.forEach((element) {
-      //   print(element['question']);
-      // });
+    FirebaseFirestore.instance
+        .collection('weights')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      List m = querySnapshot.docs;
+      wtMap = m[0]['wt_map'];
+      print(wtMap);
     });
   }
 
@@ -153,7 +190,7 @@ class _FirestoreFormState extends State<FirestoreForm> {
                           },
                         ),
                         Text(
-                          list[index]['question'],
+                          list[index]['name'],
                           style: TextStyle(fontSize: 16),
                         ),
                       ]),
@@ -167,14 +204,27 @@ class _FirestoreFormState extends State<FirestoreForm> {
   }
 
   void calculateValue() {
-    double total = 0.0;
-    for (int i = 0; i < selectedSym.length; i++) {
-      double wt = selectedSym[i]["wt"].toDouble();
-      String t = selectedSym[i]['question'];
-      double subWt = selectedSym[i]['subwts'][wtMap[t]]['w'].toDouble();
+    double total = 0;
+    allSelectedSym.forEach((element) {
+      List grs = [];
+      try {
+        grs = element['groups'];
+        String q = element['name'];
+        double wt = element['wt'].toDouble();
+        String s = "";
+        grs.forEach((e) {
+          s = "$s$e";
+          s = "$s-${ansMap["$q-$e"]},";
+        });
 
-      total = total + (wt * subWt);
-    }
+        s = s.substring(0, s.length - 1);
+
+        total = total + (wtMap[s].toDouble() * wt);
+      } catch (e) {
+        double wt = element['wt'].toDouble();
+        total = total + wt;
+      }
+    });
 
     showDialog(
       context: context,
@@ -191,21 +241,37 @@ class _FirestoreFormState extends State<FirestoreForm> {
         ],
       ),
     );
-
-    print(total);
   }
 
-  Widget getSubQuestionsView(String title, int index) {
-    List subquestion = selectedSym[index]['subwts'];
-    String sym = selectedSym[index]['question'];
-    int? v = wtMap[sym];
+  int v = 0;
+
+  Widget getSubQuestionsView() {
+    int id = selectedSymToTravarse[state - 1]['groups'][subState];
+    var subQuestion;
+    String question = selectedSymToTravarse[state - 1]['name'];
+
+    for (int i = 0; i < subQuestions.length; i++) {
+      if (subQuestions[i]['id'] == id) {
+        subQuestion = subQuestions[i];
+        break;
+      }
+    }
+    try {
+      v = ansMap["$question-$id"];
+      print(v);
+      v == null ? v = 0 : v = ansMap["$question-$id"];
+    } catch (e) {
+      v = 0;
+    }
+
+    List options = subQuestion['options'];
 
     return Column(
       children: [
         Padding(
           padding: EdgeInsets.all(10),
           child: Text(
-            "Choose a suitable option about ${sym}.",
+            "Choose a suitable option about ${question}",
             style: TextStyle(
               // color: Color(0xFFBF828A),
               fontSize: 28.0,
@@ -213,50 +279,131 @@ class _FirestoreFormState extends State<FirestoreForm> {
             ),
           ),
         ),
-        Expanded(
-          child: ListView.builder(
-              itemCount: subquestion.length,
-              itemBuilder: (BuildContext context, int i) {
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      wtMap[sym] = i;
-                      v = i;
-                    });
-                  },
-                  child: Card(
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    elevation: 3.0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(children: [
-                        Radio(
-                          value: i,
-                          groupValue: v,
-                          onChanged: (int? value) {
-                            setState(() {
-                              wtMap[sym] = value!;
-                              v = value;
-                            });
-                          },
-                          activeColor: Color(0xFFBF828A),
-                        ),
-                        Text(
-                          subquestion[i]['q'],
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                      ]),
-                    ),
+        Card(
+          margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          elevation: 3.0,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  subQuestion['name'],
+                  style: TextStyle(
+                    // color: Color(0xFFBF828A),
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
                   ),
-                );
-              }),
+                ),
+              ),
+              ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: options.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return InkWell(
+                      onTap: () {
+                        setState(() {
+                          v = i;
+                          ansMap["$question-$id"] = v;
+                          print(ansMap);
+                        });
+                      },
+                      child: Card(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        elevation: 1.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(children: [
+                            Radio(
+                              value: i,
+                              groupValue: v,
+                              onChanged: (int? value) {
+                                setState(() {
+                                  ansMap["$question-$id"] = value;
+                                  v = value!;
+                                });
+                              },
+                              activeColor: Color(0xFFBF828A),
+                            ),
+                            Text(
+                              options[i],
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ]),
+                        ),
+                      ),
+                    );
+                  }),
+            ],
+          ),
         ),
       ],
     );
+  }
+
+  void goNext() {
+    if (state == 0) {
+      setState(() {
+        state++;
+        isAtStart = false;
+      });
+    } else {
+      int noOfQuestions = selectedSymToTravarse.length;
+      List groups = selectedSymToTravarse[state - 1]['groups'];
+      int maxGroups = groups.length;
+
+      setState(() {
+        isAtStart = false;
+        if (subState < maxGroups - 1) {
+          subState++;
+        } else {
+          if (state < noOfQuestions) {
+            subState = 0;
+            state++;
+          } else {
+            isEnded = true;
+          }
+        }
+      });
+    }
+  }
+
+  void goPre() {
+    if (state == 1 && subState == 0) {
+      setState(() {
+        state = 0;
+        isAtStart = true;
+        isEnded = false;
+      });
+    } else {
+      int noOfQuestions = selectedSymToTravarse.length;
+      List groups = list[state - 1]['groups'];
+      int maxGroups = groups.length;
+
+      setState(() {
+        isEnded = false;
+        if (subState > 0) {
+          subState--;
+        } else {
+          if (state > 0) {
+            state--;
+            List groups = list[state - 1]['groups'];
+            int maxGroups = groups.length;
+
+            subState = maxGroups - 1;
+          } else {
+            isAtStart = true;
+          }
+        }
+      });
+    }
   }
 
   Widget build(BuildContext context) {
@@ -277,14 +424,14 @@ class _FirestoreFormState extends State<FirestoreForm> {
                     children: [
                       Expanded(
                           child: state > 0
-                              ? getSubQuestionsView("", state - 1)
+                              ? getSubQuestionsView()
                               : getQuestionsView(list)),
                       Padding(
                         padding: EdgeInsets.all(8),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            state > 0
+                            !isAtStart
                                 ? RaisedButton(
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(12.0),
@@ -301,7 +448,7 @@ class _FirestoreFormState extends State<FirestoreForm> {
                                     ),
                                     onPressed: () {
                                       setState(() {
-                                        state = state - 1;
+                                        goPre();
                                       });
                                     },
                                   )
@@ -316,10 +463,7 @@ class _FirestoreFormState extends State<FirestoreForm> {
                                   horizontal: 15.0, vertical: 15.0),
                               color: Color(0xFFBF828A),
                               child: Text(
-                                (selectedSym.length == 0 ||
-                                        selectedSym.length > state)
-                                    ? "Next"
-                                    : "Finish",
+                                !isEnded ? "Next" : "Finish",
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 16.0,
@@ -327,7 +471,8 @@ class _FirestoreFormState extends State<FirestoreForm> {
                               ),
                               onPressed: () {
                                 if (state == 0) {
-                                  selectedSym.clear();
+                                  allSelectedSym.clear();
+                                  selectedSymToTravarse.clear();
                                   if (checkList
                                           .where((val) => val == true)
                                           .length ==
@@ -340,22 +485,26 @@ class _FirestoreFormState extends State<FirestoreForm> {
                                   }
                                   for (int i = 0; i < list.length; i++) {
                                     if (checkList[i]) {
-                                      selectedSym.add(list[i]);
-                                      if (wtMap[list[i]['question']] == null) {
-                                        wtMap[list[i]['question']] = 0;
-                                      }
+                                      allSelectedSym.add(list[i]);
+                                      try {
+                                        list[i]['groups'];
+                                        selectedSymToTravarse.add(list[i]);
+                                      } catch (e) {}
                                     }
                                   }
-                                  print(selectedSym);
                                 }
-                                setState(() {
-                                  if (state == 0 ||
-                                      state < selectedSym.length) {
-                                    state = state + 1;
-                                  } else {
-                                    calculateValue();
-                                  }
-                                });
+                                if (selectedSymToTravarse.length == 0 &&
+                                    allSelectedSym.length > 0) {
+                                      calculateValue();
+                                } else {
+                                  setState(() {
+                                    if (!isEnded) {
+                                      goNext();
+                                    } else {
+                                      calculateValue();
+                                    }
+                                  });
+                                }
                               },
                             ),
                           ],
@@ -389,7 +538,7 @@ class _FirestoreFormState extends State<FirestoreForm> {
                         } else {
                           setState(() {
                             _newVoiceText =
-                                'Choose a suitable option about ${selectedSym[state - 1]['question']}';
+                                'Choose a suitable option about ${selectedSymToTravarse[state - 1]['question']}';
                           });
                         }
 
